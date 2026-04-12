e77a6357fa391249d7db66243779aea23611ded9
jmontero22c <jesusda71.2@gmail.com>
2026-04-11 20:47:16 -0500

    Cambio de comillas dobles a simples

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -29,7 +29,7 @@ async function fetchAPI(endpoint, options = {}) {
         const response = await fetch(`${API_BASE}${endpoint}`, {
             headers: {
                 'Content-Type': 'application/json',
-                "ngrok-skip-browser-warning": "69420"
+                'ngrok-skip-browser-warning': '69420'
             },
             ...options
         });
